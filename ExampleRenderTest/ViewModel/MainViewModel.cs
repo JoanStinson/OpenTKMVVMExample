a2c39e9c9a45fed233ba200b52d18f1ad1bd2314
Joan Ginard Mateo <jgm7.hd@gmail.com>
2024-06-10 17:44:07 +0200

    Rendering four slots

--- ExampleRenderTest/ViewModel/MainViewModel.cs
+++ ExampleRenderTest/ViewModel/MainViewModel.cs
@@ -5,10 +5,16 @@ namespace ExampleRenderTest.ViewModel
     public class MainViewModel
     {
         public Slot1ViewModel Slot1ViewModel { get; }
+        public Slot1ViewModel Slot2ViewModel { get; }
+        public Slot1ViewModel Slot3ViewModel { get; }
+        public Slot1ViewModel Slot4ViewModel { get; }
 
         public MainViewModel()
         {
             Slot1ViewModel = new Slot1ViewModel();
+            Slot2ViewModel = new Slot1ViewModel();
+            Slot3ViewModel = new Slot1ViewModel();
+            Slot4ViewModel = new Slot1ViewModel();
         }
     }
 }
